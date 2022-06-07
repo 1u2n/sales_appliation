697c3209d55def9d2c48b0979cc62e1a3cbb0c16
1u2n <87938709+1u2n@users.noreply.github.com>
2022-06-07 15:42:48 +0530

    Add files via upload

---
+++ Sales Application.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.title("Sales Application")
+root.geometry("400x400")
+
+month = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+print(month)
+
+profits = ("96000", "58000", "73000", "29000", "34000", "63000", "81000", "46000", "13000", "73000", "55000", "91000")
+print(profits)
+
+root.mainloop()
